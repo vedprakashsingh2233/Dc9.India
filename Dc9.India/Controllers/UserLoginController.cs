a610f08479551377853dcff96934128a308ea0db
vedprakashsingh2233 <vedprakashsingh2233@gmail.com>
2024-10-21 19:06:07 +0530

    coomit

---
+++ Dc9.India/Controllers/UserLoginController.cs
@@ -0,0 +1,80 @@
+﻿using Dc9.India.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dc9.India.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+       [HttpPost]
+        public JsonResult InsertUserDetails( string UserName, string EmailId, string Password, string MobileNo)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic["Result"] = "";           
+            try
+            {
+                string[,] Param = new string[,]
+                {
+                    {"@Action","Registration" },
+                    {"@UserName",UserName.ToString().Trim() },
+                    {"@EmailId",EmailId.Trim() },
+                    {"@Password", CommonMethod.Encrypt(CommonMethod.SecureHashSHA256(Password.Trim()),"") },
+                    {"@MobileNo",MobileNo.Trim() },
+                };
+                DataTable dt = CommonMethod.ExecuteProc("UserDetail_Regisration_Login_SP", Param);
+                if (dt.Rows.Count > 0)
+                {
+                    dic["Result"] = dt.Rows[0]["Msg"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                dic["Result"] = ex.Message;
+            }
+            return Json(dic);
+        }
+        [HttpPost]
+        public JsonResult LoginDetail(string UserCode, string Password)
+        {
+            DataTable DtLoginInfo = new DataTable();
+            Dictionary<string, string> Dic = new Dictionary<string, string>();
+            Dic["Result"] = "";
+            try
+            {
+                string[,] param = new string[,]{
+                            {"@EmailId",UserCode.Trim()},
+                            {"@Password",CommonMethod.Encrypt(CommonMethod.SecureHashSHA256(Password.Trim()),"")},
+                            {"@Action","Login" },
+                        };
+                DtLoginInfo = CommonMethod.ExecuteProc("UserDetail_Regisration_Login_SP", param);
+                if (DtLoginInfo.Rows.Count > 0)
+                {
+                    if (DtLoginInfo.Rows[0]["Msg"].ToString() == "Success") 
+                    { 
+                                    
+                        Session["UserId"] = DtLoginInfo.Rows[0]["UserId"].ToString();
+                        Session["UserEmailId"] = DtLoginInfo.Rows[0]["Emailid"].ToString();
+                        Dic["Result"] = DtLoginInfo.Rows[0]["Msg"].ToString();
+                    }
+                    else
+                    {
+                        Dic["Result"] = DtLoginInfo.Rows[0]["Msg"].ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Dic["Result"] = ex.Message;
+            }
+            return Json(Dic);
+        }
+
+
+    }
+}
---
+++ Dc9.India/Models/CommonMethod.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Dc9.India.Models
+{
+    public class CommonMethod
+    {
+        public static DataTable ExecuteProc(string Proc, String[,] arr)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Constr"].ToString());
+            SqlCommand com = new SqlCommand(Proc, conn);
+            com.CommandType = System.Data.CommandType.StoredProcedure;
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                com.Parameters.AddWithValue(arr[i, 0], arr[i, 1]);
+            }
+            SqlDataAdapter ad = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            return dt;
+        }
+        public static string Encrypt(string Value, string EncryptionKey)
+        {
+            if (EncryptionKey.Trim() == "")
+                EncryptionKey = "AMRESH";
+            byte[] bytes = Encoding.Unicode.GetBytes(Value);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(EncryptionKey, new byte[13]
+                {
+          (byte) 73,
+          (byte) 118,
+          (byte) 97,
+          (byte) 110,
+          (byte) 32,
+          (byte) 77,
+          (byte) 101,
+          (byte) 100,
+          (byte) 118,
+          (byte) 101,
+          (byte) 100,
+          (byte) 101,
+          (byte) 118
+                });
+                aes.Key = rfc2898DeriveBytes.GetBytes(32);
+                aes.IV = rfc2898DeriveBytes.GetBytes(16);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(bytes, 0, bytes.Length);
+                        cryptoStream.Close();
+                    }
+                    Value = Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+            return Value;
+        }
+
+        public static string Decrypt(string Value, string EncryptionKey)
+        {
+            if (EncryptionKey.Trim() == "")
+                EncryptionKey = "AMRESH";
+            byte[] buffer = Convert.FromBase64String(Value);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(EncryptionKey, new byte[13]
+                {
+          (byte) 73,
+          (byte) 118,
+          (byte) 97,
+          (byte) 110,
+          (byte) 32,
+          (byte) 77,
+          (byte) 101,
+          (byte) 100,
+          (byte) 118,
+          (byte) 101,
+          (byte) 100,
+          (byte) 101,
+          (byte) 118
+                });
+                aes.Key = rfc2898DeriveBytes.GetBytes(32);
+                aes.IV = rfc2898DeriveBytes.GetBytes(16);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(buffer, 0, buffer.Length);
+                        cryptoStream.Close();
+                    }
+                    Value = Encoding.Unicode.GetString(memoryStream.ToArray());
+                }
+            }
+            return Value;
+        }
+        public static string SecureHashSHA256(string Password) => GetStringFromHash(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(Password)));
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int index = 0; index < hash.Length; ++index)
+                stringBuilder.Append(hash[index].ToString("X2"));
+            return stringBuilder.ToString();
+        }
+    }
+}
