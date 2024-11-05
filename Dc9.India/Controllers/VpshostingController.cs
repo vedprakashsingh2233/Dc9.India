10c1edbef9ea3e5325efc3768c02d6fe861a2354
vedprakashsingh2233 <vedprakashsingh2233@gmail.com>
2024-11-05 22:41:03 +0530

    date 05-11-24

---
+++ Dc9.India/Controllers/VpshostingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dc9.India.Controllers
+{
+    public class VpshostingController : Controller
+    {
+        // GET: Vpshosting
+        public ActionResult ManagedVps()
+        {
+            return View();
+        }
+    }
+}
