15bb9ad6fe4436af785673e73406d6b3c6230f7e
vedprakashsingh2233 <vedprakashsingh2233@gmail.com>
2025-01-02 21:57:33 +0530

    commit from home

--- Dc9.India/Controllers/VpshostingController.cs
+++ Dc9.India/Controllers/VpshostingController.cs
@@ -248,13 +248,11 @@ namespace Dc9.India.Controllers
                 {
                     StringBuilder htmlString = new StringBuilder();
                     for (int i = 0; i < dt.Rows.Count; i++)
-                    {
-                       
-
+                    {                    
                         htmlString.AppendLine("<div class=\"col-sm-6 item\">");
-                        htmlString.AppendLine("    <label class=\"col-sm-8\">Antivirus A</label>");
-                        htmlString.AppendLine("    <input type=\"checkbox\" class=\"form-check col-sm-3 chkAddtional\" value=\"Antivirus A\" />");
-                        htmlString.AppendLine("    <span class=\"text-extra-dark-gray alt-font font-weight-600 mb-0\">$<span class=\"AddtionalPrice\">3</span>/mo</span>");
+                        htmlString.AppendLine("<label class=\"col-sm-8\">" + dt.Rows[i]["ItemName"] + "</label>");
+                        htmlString.AppendLine("<input type=\"checkbox\" class=\"form-check col-sm-3 chkAddtional\" value=\""+ dt.Rows[i]["ItemName"] + "\" />");
+                        htmlString.AppendLine("<span class=\"text-extra-dark-gray alt-font font-weight-600 mb-0\">$<span class=\"AddtionalPrice\">" + dt.Rows[i]["ItemPrice"] + "</span>/mo</span>");
                         htmlString.AppendLine("</div>");
                     }
                     dic["Data"] = htmlString.ToString();
